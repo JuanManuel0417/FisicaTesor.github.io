60e6622f51412a5de15a796f3d3cf0d8b1b745bb
Juanma <gonzalezmejiaj264@gmail.com>
2023-11-13 18:57:44 -0500

    Finalización

--- java/logic.js
+++ java/logic.js
@@ -10,23 +10,23 @@ const acertijos = [
 
     { pregunta: "Personas vestidas de blanco atraen nuestra atención, con aromas deliciosos roban nuestro corazón.", respuestas: ["54"] },
 
-    { pregunta: ":¿A qué velocidad debe circular un auto de carreras para recorrer 113 km en un cuarto de hora?", respuestas: ["125,555 m/s"] },
+    { pregunta: ":¿A qué velocidad debe circular un auto de carreras para recorrer 113 km en un cuarto de hora?", respuestas: ["125,555m/s"] },
 
-    { pregunta: "Lugar lleno de líricas invadido de silencio, lugar asombroso que no valoramos, textos polvorientos que al ver nos da asco.", respuestas: ["4.2x10 a la 10km"] },
+    { pregunta: "Lugar lleno de líricas invadido de silencio, lugar asombroso que no valoramos, textos polvorientos que al ver nos da asco.", respuestas: ["4,2x10 a la 10km"] },
 
     { pregunta: "Gigante calvo, gordo y gruñón, gigante ausente, feo y mandón, gigante relacionado con el orden de la institución ¿Qué lugar será?", respuestas: ["Hz"] },
 
     { pregunta: " Espacio de mandones, gordos, altos, viejos y pequeños, espacio de posibles soluciones en donde se ruega por lo imposible.", respuestas: ["4890HGKRTY67000DFTHJKNM…KL-MALO"] },
 
-    { pregunta: "Un sicario dispara una pistola de la cual sale una bala disparada a 573 m/s, y este mismo lleva un ángulo de elevación de 62° respecto a la horizontal. Calcula la ALTURA máxima que obtuvo la bala", respuestas: ["13513,687 m"] },
+    { pregunta: "Un sicario dispara una pistola de la cual sale una bala disparada a 573 m/s, y este mismo lleva un ángulo de elevación de 62° respecto a la horizontal. Calcula la ALTURA máxima que obtuvo la bala", respuestas: ["13513,687m"] },
 
-    { pregunta: "Un sicario dispara una pistola de la cual sale una bala disparada a 573 m/s, y este mismo lleva un ángulo de elevación de 62°, respecto a la horizontal. AHORA CALCULA EL ALCANCE.", respuestas: ["65179,399 m "] },
+    { pregunta: "Un sicario dispara una pistola de la cual sale una bala disparada a 573 m/s, y este mismo lleva un ángulo de elevación de 62°, respecto a la horizontal. AHORA CALCULA EL ALCANCE.", respuestas: ["65179,399m"] },
 
     { pregunta: "Calma nuestra sed en las mañanas activas y llenas de calor.", respuestas: ["f=1/T"] },
 
     { pregunta: "Fila eterna para pedir fiado ¿Qué será?", respuestas: ["VHGIOTRT"] },
 
-    { pregunta: " Un objeto realiza un movimiento circular uniforme acelerado y describe un diámetro de 0.8 m y efectúa una vuelta completa en 2.0 segundos. Calcula la velocidad Lineal", respuestas: ["12,566 m/s"] },
+    { pregunta: " Un objeto realiza un movimiento circular uniforme acelerado y describe un diámetro de 0.8 m y efectúa una vuelta completa en 2.0 segundos. Calcula la velocidad Lineal", respuestas: ["12,566m/s"] },
 
     { pregunta: " Lugar desagradable, lugar en donde pasamos 6 horas sentado, lugar que pintamos y a los dos días despintamos, ¿qué raro no?", respuestas: ["PERDISTE_EL_AÑO_OME_MALO."] },
 
